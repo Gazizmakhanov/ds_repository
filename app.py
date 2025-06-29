26214faf573e5d4751d689f768f9b43362e1caae
Gazizmakhanov <37010988+Gazizmakhanov@users.noreply.github.com>
2025-06-28 18:23:25 -0700

    updated files

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ import pandas as pd
 import plotly.express as px
 
 df = pd.read_csv('vehicles_us.csv')
-st.header('Example')
+st.header('Welcome to my first app on render!')
 fig = px.scatter(df, x='price', y='model_year')
 st.plotly_chart(fig)
 
+fig = px.histogram(df, x='cylinders', nbins=6, title='Histogram of Values')
+st.plotly_chart(fig, use_container_width=True)
+
+use_more_bins = st.checkbox("Use more bins", value=False)
+
+nbins = 15 if use_more_bins else 5
+fig = px.histogram(df, x='cylinders', nbins=nbins, title=f'Histogram with {nbins} Bins')
+st.plotly_chart(fig, use_container_width=True)
